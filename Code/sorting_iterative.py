98b9aa4f2277d9bf52b7128778f53b73710df168
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-10-25 18:20:27 +0530

    added sorting algorithms solutions

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -1,12 +1,16 @@
 #!python
-
+def swap(i, j, items):
+    items[i], items[j] = items[j], items[i]
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
-
+    for i in range(len(items)-1):
+        if items[i] > items[i+1]:
+            return False
+    return True
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
@@ -15,6 +19,11 @@ def bubble_sort(items):
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Repeat until all items are in sorted order
     # TODO: Swap adjacent items that are out of order
+    for _ in range(len(items)):
+        for j in range(len(items)-1):
+            if items[j] > items[j+1]:
+                items[j], items[j+1] = items[j+1], items[j]
+    return items
 
 
 def selection_sort(items):
@@ -25,6 +34,14 @@ def selection_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
+    for i in range(len(items)-1):
+        smallest = i
+        for j in range(i+1, len(items)):
+            if items[smallest] > items[j]:
+                smallest = j
+        swap(smallest, i, items)
+    return items
+
 
 
 def insertion_sort(items):
@@ -35,3 +52,9 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+    for i in range(1, len(items)):
+        j = i
+        while j > 0 and items[j] < items[j-1]:
+            swap(j, j-1, items)
+            j -= 1
+    return items
